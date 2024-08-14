861f25151ed53576c82288973e230483fbf0aadd
Finley <finley.mwachia12@gmail.com>
2024-08-14 12:20:13 +0300

    app jsx tested and working

--- src/index.jsx
+++ src/index.jsx
@@ -9,10 +9,7 @@
 //6. Make sure that the final website is styled like the example shown here:
 //https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
